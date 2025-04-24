e846cf7078df27fa50b5b16a45c219e82294dfac
Nico Bruzewicz <nico.bruzewicz@thelangschool.net>
2025-04-24 09:23:28 -0400

    i made grade weights for quiz and nti. checkedtype is supposed to be what the thing is, whether it be HW, Quiz, or Final. amountofstufftoaverage is what you divide unaveragedgpa by. checkedthing is the grade or NTI.

---
+++ Main.py
@@ -0,0 +1,13 @@
+import random
+if checkedthing == "NTI":
+    gradeforcheck = random.randint(1,100)
+    if gradeforcheck >= 80:
+        gradeforcheck = random.randint(1, 100)
+    if gradeforcheck >= 65:
+        gradeforcheck = random.randint(1, 100)
+    unaveragedgpa += gradeforcheck
+    amountofstufftoaverage += 1
+if checkedtype == "Quiz"
+    gradeforcheck = checkedthing
+    unaveragedgpa += gradeforcheck * 4
+    amountofstufftoaverage += 4
