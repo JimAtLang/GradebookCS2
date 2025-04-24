a02e20b3a094c28bfa2b6f0b36bd0bf869b3b716
Max Fekete <max.fekete@thelangschool.net>
2025-04-24 09:34:14 -0400

    Refined code

--- Main.py
+++ Main.py
@@ -16,18 +16,48 @@ names_a = {
     'CW3': 100,
     'CW4': 100
 }
+names_b = {
+    'HW1': 95,
+    'HW2': 85
+}
+names_c = {
+    'Test1': 88,
+    'Test2': 95
+}
+names_d = {
+    'Final': 91.33
+}
 
-print('700 Jesus Ramirez')
+student_name = 'Jesus Ramirez'
+
+print(student_name)
 time.sleep(1)
+print(f'\nCurrent Average: {(((sum(names_a.values())) / len(names_a)) * (aw/(aw+bw+cw+dw)) * 100) }n')
 print(f'\nGrade Detail\n----------')
 time.sleep(2)
-print(f'\nSection       Weighting     Pts Earned    Max Pts       Average')
+print(f'Section       Weighting     Pts Earned    Max Pts       Average')
 time.sleep(0.5)
-while len(a) != 14:
-    a += ' '
-a += f'{(aw / (aw + bw + cw + dw)) * 100:.2f}'
-a += '%'
-while len(a) != 28:
-    a += ' '
-a += f'{}'
-print(a)
+
+def print_section(section_name, section_weight, section_grades):
+    line = section_name
+    while len(line) != 14:
+        line += ' '
+    line += f'{(section_weight / (aw + bw + cw + dw)) * 100:.2f}%'
+    while len(line) != 28:
+        line += ' '
+    line += f'{sum(section_grades.values()):.2f}'
+    while len(line) != 42:
+        line += ' '
+    line += f'{len(section_grades) * 100:.2f}'
+    while len(line) != 56:
+        line += ' '
+    line += f'{sum(section_grades.values()) / len(section_grades):.2f}'
+    print(line)
+
+print_section(a, aw, names_a)
+time.sleep(0.5)
+print_section(b, bw, names_b)
+time.sleep(0.5)
+print_section(c, cw, names_c)
+time.sleep(0.5)
+print_section(d, dw, names_d)
