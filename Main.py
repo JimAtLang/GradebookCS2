70dc1aed33d5ce3a3ccb4bc98ffd469ef104b1d0
Nico Bruzewicz <nico.bruzewicz@thelangschool.net>
2025-04-25 09:03:19 -0400

    hi guys i added more :3

--- Main.py
+++ Main.py
@@ -7,7 +7,29 @@ if checkedthing == "NTI":
         gradeforcheck = random.randint(1, 100)
     unaveragedgpa += gradeforcheck
     amountofstufftoaverage += 1
-if checkedtype == "Quiz"
-    gradeforcheck = checkedthing
+if checkedtype == "Quiz":
+    if not checkedthing == "NTI":
+        gradeforcheck = checkedthing
     unaveragedgpa += gradeforcheck * 4
     amountofstufftoaverage += 4
+if checkedtype == "HW":
+    if not checkedthing == "NTI":
+        gradeforcheck = checkedthing
+    unaveragedgpa += gradeforcheck
+    amountofstufftoaverage += 1
+if checkedtype == "Test":
+    if not checkedthing == "NTI":
+        gradeforcheck = checkedthing
+    unaveragedgpa += gradeforcheck * 6
+    amountofstufftoaverage += 6
+if checkedtype == "Project":
+    if not checkedthing == "NTI":
+        gradeforcheck = checkedthing
+    unaveragedgpa += gradeforcheck * 10
+    amountofstufftoaverage += 10
+if checkedtype == "Final":
+    gradeforcheck = -100
+    if not checkedthing == "NTI":
+        gradeforcheck = checkedthing
+    unaveragedgpa += gradeforcheck * 15
+    amountofstufftoaverage += 15
