683390f3a0bb01f5e23699ee639300bedfe31463
Nico Bruzewicz <nico.bruzewicz@thelangschool.net>
2025-05-01 09:05:30 -0400

    shnanigans

---
+++ code.py
@@ -0,0 +1,35 @@
+import random
+import math
+def gradeweight(checkedthing,checkedtype):
+    if checkedthing == "NTI":
+        gradeforcheck = random.randint(1,100)
+        if gradeforcheck >= 80:
+            gradeforcheck = random.randint(1, 100)
+        if gradeforcheck >= 65:
+            gradeforcheck = random.randint(1, 100)
+    if checkedtype == "Quiz":
+        if not checkedthing == "NTI":
+            gradeforcheck = checkedthing
+        unaveragedgpa += gradeforcheck * 4
+        amountofstufftoaverage += 4
+    if checkedtype == "HW":
+        if not checkedthing == "NTI":
+            gradeforcheck = checkedthing
+        unaveragedgpa += gradeforcheck
+        amountofstufftoaverage += 1
+    if checkedtype == "Test":
+        if not checkedthing == "NTI":
+            gradeforcheck = checkedthing
+        unaveragedgpa += gradeforcheck * 6
+        amountofstufftoaverage += 6
+    if checkedtype == "Project":
+        if not checkedthing == "NTI":
+            gradeforcheck = checkedthing
+        unaveragedgpa += gradeforcheck * 10
+        amountofstufftoaverage += 10
+    if checkedtype == "Final":
+        gradeforcheck = -100
+        if not checkedthing == "NTI":
+            gradeforcheck = checkedthing
+        unaveragedgpa += gradeforcheck * 15
+        amountofstufftoaverage += 15
