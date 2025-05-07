0db54a92e715fe20c177bea946b2610f74e8f7bc
Nico Bruzewicz <nico.bruzewicz@thelangschool.net>
2025-05-07 09:18:04 -0400

    fixed stuff

--- code.py
+++ code.py
@@ -9,27 +9,27 @@ def gradeweight(checkedthing,checkedtype,unaveragedgpa,amountofstufftoaverage):
             gradeforcheck = random.randint(1, 100)
         if gradeforcheck >= 65:
             gradeforcheck = random.randint(1, 100)
-    if checkedtype == "Quiz":
+    if checkedtype == "Quiz1" or checkedtype == "Quiz2" or checkedtype == "Quiz3" or checkedtype == "Quiz4":
         if not checkedthing == "NTI":
             gradeforcheck = checkedthing
         unaveragedgpa += gradeforcheck * 4
         amountofstufftoaverage += 4
-    if checkedtype == "HW":
+    if checkedtype == "HW1" or checkedtype == "HW2" or checkedtype == "HW3" or checkedtype == "HW4" or checkedtype == "HW5" or checkedtype == "HW6" or checkedtype == "HW7" or checkedtype == "HW8" or checkedtype == "HW9" or checkedtype == "HW10":
         if not checkedthing == "NTI":
             gradeforcheck = checkedthing
         unaveragedgpa += gradeforcheck
         amountofstufftoaverage += 1
-    if checkedtype == "Test":
+    if checkedtype == "Test1" or checkedtype == "Test2":
         if not checkedthing == "NTI":
             gradeforcheck = checkedthing
         unaveragedgpa += gradeforcheck * 6
         amountofstufftoaverage += 6
-    if checkedtype == "Project":
+    if checkedtype == "Project1" or checkedtype == "Project2":
         if not checkedthing == "NTI":
             gradeforcheck = checkedthing
         unaveragedgpa += gradeforcheck * 10
         amountofstufftoaverage += 10
-    if checkedtype == "Final":
+    if checkedtype == "Final1":
         gradeforcheck = -100
         if not checkedthing == "NTI":
             gradeforcheck = checkedthing
