35ace8e1bba8d37eb5dfbd689de5084a9edd9a36
Rosa Simms <rosa.simms@thelangschool.net>
2025-05-07 09:24:58 -0400

    split line

--- readfile.py
+++ readfile.py
@@ -1,7 +1,10 @@
-with open("data\\Algebra 1") as file:
-    lines = file.readlines()
-    print(lines)
-    gerbilbone = "ifyourreadingthisiminyourwalls"
-    print(gerbilbone)
-    for line in lines:
-        gerbilbone = line.pop(0)
+def readfile(filename, )
+    with open("data\\"+filename) as file:
+        lines = file.readlines()
+        print(lines)
+        gerbilbone = "ifyourreadingthisiminyourwalls"
+        print(gerbilbone)
+        for line in lines:
+            gerbilbone = line.split(",")
+
+
