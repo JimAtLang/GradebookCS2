19073fe7b12fd2da2e5028445063a01b75a5c323
62maxf <max.fekete@students.thelangschool.org>
2025-05-08 09:29:10 -0400

    $ sq 100

--- Main.py
+++ Main.py
@@ -1,17 +1,37 @@
 import time
 import random
+import math
 
-avg = 0
+try:
+    sq = input('').split(' ')[2]
+    print(f'Sqwimble set to {sq}')
+except IndexError:
+    print('male sonic')
 
-a = 'Classwork'
-aw = 999
-b = 'Homework'
-bw = 1
-c = 'Tests'
-cw = 3
-d = 'Final'
-dw = 3
+def nto4(n):
+    if n > 97:
+        return 4.0
+    if n < 57:
+        return 1.0
+    return f'{(n - 57) / 10:.1f}'
 
+def print_section(n, w, g):  # name, weighting, grades
+    l = n.ljust(14)
+    l += f'{(w / (aw + bw + cw + dw)) * 100:.2f}%'.ljust(14)
+    l += f'{sum(g.values()):.2f}'.ljust(14)
+    l += f'{len(g) * 100:.2f}'.ljust(14)
+    l += f'{sum(g.values()) / len(g):.2f}'
+    print(l)
+
+# Student and weights
+student_name = 'Jesus Ramirez'
+
+a, aw = 'Classwork', 3
+b, bw = 'Homework', 1
+c, cw = 'Tests', 3
+d, dw = 'Final', 3
+
+# Grade dictionaries
 names_a = {
     'CW1': 100,
     'CW2': 87.5,
@@ -30,47 +50,15 @@ names_d = {
     'Final': 91.33
 }
 
-student_name = 'Jesus Ramirez'
-
-def nto4(n):
-    if n < 60:
-        return 1
-    if n < 63:
-        return 1.1
-    if n < 66:
-        return 1.2
-    if n < 69:
-        return 1.3
-    # shit i don't know how the grading scheme works
-
-def w(g, h):
-    avgb = avg
-    avgb += sum(g.values()) / len(g) * (h / (aw + bw + cw + dw)) * 100
-    avg = avgb
-
+# Output
 print(student_name)
 time.sleep(1)
+overall_grade = 94.83
+print(f'\nCurrent Grade\n{overall_grade}\n{nto4(overall_grade)}')
 print(f'\nGrade Detail\n----------')
 time.sleep(2)
 print(f'Section       Weighting     Pts Earned    Max Pts       Average')
 time.sleep(0.5)
-
-def print_section(n, w, g): # name, weighting, grades
-    l = n # First thing is the name
-    while len(l) != 14:
-        l += ' ' # Even spacing
-    l += f'{(w / (aw + bw + cw + dw)) * 100:.2f}%' # Weighting as a %
-    while len(l) != 28:
-        l += ' ' # Even spacing, the sequel
-    l += f'{sum(g.values()):.2f}' # Points earned
-    while len(l) != 42:
-        l += ' ' # Even spacing, the sequel, the sequel
-    l += f'{len(g) * 100:.2f}' # Points possible
-    while len(l) != 56:
-        l += ' ' # Even spacing, the sequel, the sequel, the sequel
-    l += f'{sum(g.values()) / len(g):.2f}' # The average
-    print(l) # Looks just like veracross
-
 print_section(a, aw, names_a)
 time.sleep(0.5)
 print_section(b, bw, names_b)
