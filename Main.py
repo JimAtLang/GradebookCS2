fb12f92682efeae2e356e946a907a50c0f76ec74
62maxf <max.fekete@students.thelangschool.org>
2025-05-14 09:03:46 -0400

    child

--- Main.py
+++ Main.py
@@ -21,14 +21,6 @@ def nto4(n):
         return 1.0
     return f'{(n - 57) / 10:.1f}'
 
-def print_section(n, w, g):  # name, weighting, grades
-    l = n.ljust(18)
-    l += f'{(w / (aw + bw + cw + dw + ew)) * 100:.2f}%'.ljust(18)
-    l += f'{sum(g.values()):.2f}'.ljust(18)
-    l += f'{len(g) * 100:.2f}'.ljust(18)
-    l += f'{sum(g.values()) / len(g):.2f}'
-    print(l)
-
 student_name = 'Jesus Ramirez'
 
 a, aw = 'Quiz', 3
--- wholemilk.py
+++ wholemilk.py
@@ -1,6 +1,6 @@
 def print_section(n, w, g):  # name, weighting, grades
     l = n.ljust(18)
-    l += f'{(w / (aw + bw + cw + dw)) * 100:.2f}%'.ljust(18)
+    l += f'{(w / (aw + bw + cw + dw + ew)) * 100:.2f}%'.ljust(18)
     l += f'{sum(g.values()):.2f}'.ljust(18)
     l += f'{len(g) * 100:.2f}'.ljust(18)
     l += f'{sum(g.values()) / len(g):.2f}'
