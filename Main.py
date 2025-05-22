4189cbc9f8b01c82fdede1a3662fdd4a66cd7986
62maxf <max.fekete@students.thelangschool.org>
2025-05-22 09:38:34 -0400

    6 jan 2021

--- Main.py
+++ Main.py
@@ -95,7 +95,7 @@ def nto4(n):
         return 'Failure'
     return f'{(n - 57) / 10:.1f}'
 
-t = 10
+t = 0.3
 
 print(student_name)
 time.sleep(1)
@@ -103,17 +103,14 @@ overall_grade = 20
 print(f'\nCurrent Grade\n{overall_grade}\n{nto4(overall_grade)}')
 print(f'\nGrade Detail\n----------')
 time.sleep(2)
-while True:
-    print(f'Section           Weighting         Pts Earned        Max Pts           Average')
-    time.sleep(t)
-    print_section(a, aw, names_a)
-    time.sleep(t)
-    print_section(b, bw, names_b)
-    time.sleep(t)
-    print_section(c, cw, names_c)
-    time.sleep(t)
-    print_section(d, dw, names_d)
-    time.sleep(t)
-    print_section(e, ew, names_e)
-    t -= 0.1
-    print(f'{t:.1f}')
+print(f'Section           Weighting         Pts Earned        Max Pts           Average')
+time.sleep(t)
+print_section(a, aw, names_a)
+time.sleep(t)
+print_section(b, bw, names_b)
+time.sleep(t)
+print_section(c, cw, names_c)
+time.sleep(t)
+print_section(d, dw, names_d)
+time.sleep(t)
+print_section(e, ew, names_e)
