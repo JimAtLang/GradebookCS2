99a6933c5943299729c3a5956d570ff56a9d8049
Rosa Simms <rosa.simms@thelangschool.net>
2025-06-04 09:24:30 -0400

    input function

---
+++ input.py
@@ -0,0 +1,5 @@
+def getstudent():
+    inp = input('Select a student: ')
+    clas = input('Select a class they are in: ')
+    return (inp, clas)
+getstudent()
--- readfile.py
+++ readfile.py
@@ -5,13 +5,11 @@ def readfile(filename):
     trololololol = []
     with open("data\\"+filename) as file:
         lines = file.readlines()
-        headers = lines[0].split(",")
         for line in lines:
             bleed = line.split(",")
             if bleed[0]=="Name":
                 continue
-            Grades = [bleed.pop()]
-            for i in range(19):
-                Grades.append(bleed.pop())
-            trololololol.append(Grades)
-    return trololololol
+            trololololol.append(bleed)
+
+    \
+return trololololol
