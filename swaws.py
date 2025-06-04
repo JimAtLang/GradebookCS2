6b7fb4282f955820b3b31f998348a7ed85557bf0
Nico Bruzewicz <nico.bruzewicz@thelangschool.net>
2025-06-04 09:27:09 -0400

    added a findgrade function

--- swaws.py
+++ swaws.py
@@ -16,5 +16,20 @@ def finalfinal(gradelistlist,lengthoffile):
             (totalgrade,denominator) = gradeweight((gradelistlist[f])[i],types[i],totalgrade,denominator)
         GPALIST[f] = (gradelistlist[f][0],100 * totalgrade/denominator,)
     return GPALIST
-grades = finalfinal(readfile("Algebra 1"),103)
-print(grades)
+#grades = finalfinal(readfile("Algebra 1"),103)
+#print(grades)
+
+def findgrade(tuple):
+    pair = (0,0)
+    with open("data\\" + tuple[1]) as file:
+        lines = file.readlines()
+        linecount = len(lines)
+        list = finalfinal(readfile(tuple[1]),linecount-2)
+        for i in list:
+            if i[0] == tuple[0]:
+                pair = i
+    if pair != (0,0):
+        return(pair)
+    return(tuple[0],"is not in",tuple[1])
+
+print(findgrade(("Jesus Brown","Algebra 1")))
